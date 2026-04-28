a3164a3db282e7b0bf7df6b4d6a724337a095bf1
khhungad-ui <khhungad@gmail.com>
2026-04-28 14:10:26 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ name = st.text_input("Enter your name:")
 
 
 if name != "":
-          name.label_visibility = "hidden"
+          name.label_visibility("hidden")
           # -------------------------------
           # Title and Description
           # -------------------------------
